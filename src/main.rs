40c08561d6c1e27cba747a0b3e20b5b11b01f5d3
Jacek Grzybowski <totospl99@gmail.com>
2023-10-24 22:57:13 +0200

    triangles example

--- src/main.rs
+++ src/main.rs
@@ -16,31 +16,40 @@ impl Point {
     }
 }
 
-fn main() {
-    let imgx = 800;
-    let imgy = 800;
-
-    let a = Point::new(100.0, 700.0);
-    let b = Point::new(400.0, 100.0);
-    let c = Point::new(700.0, 700.0);
+fn draw_triangle_equilateral(
+    buf: &mut image::ImageBuffer<image::Rgb<u8>, Vec<u8>>,
+    height: f32,
+    center: Point,
+    angle: f32,
+    color: image::Rgb<u8>,
+) {
+    let mag = (2.0 / 3.0) * height;
+    let c = Point::new(
+        center.x + mag * f32::sin(0.0 + angle),
+        center.y + mag * f32::cos(0.0 + angle),
+    );
+    let b = Point::new(
+        center.x + mag * f32::sin(2.0 * std::f32::consts::FRAC_PI_3 + angle),
+        center.y + mag * f32::cos(2.0 * std::f32::consts::FRAC_PI_3 + angle),
+    );
+    let a = Point::new(
+        center.x + mag * f32::sin(4.0 * std::f32::consts::FRAC_PI_3 + angle),
+        center.y + mag * f32::cos(4.0 * std::f32::consts::FRAC_PI_3 + angle),
+    );
 
     let vec_ab = Point::new(a.x - b.x, a.y - b.y);
     let vec_bc = Point::new(b.x - c.x, b.y - c.y);
     let vec_ca = Point::new(c.x - a.x, c.y - a.y);
 
-    // Create a new ImgBuf with width: imgx and height: imgy
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+    let min_x = a.x.min(f32::min(b.x, c.x)) as u32;
+    let max_x = a.x.max(f32::max(b.x, c.x)) as u32;
+    let min_y = a.y.min(f32::min(b.y, c.y)) as u32;
+    let max_y = a.y.max(f32::max(b.y, c.y)) as u32;
 
-    // Iterate over the coordinates and pixels of the image
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = (0.3 * x as f32) as u8;
-        let b = (0.3 * y as f32) as u8;
-        *pixel = image::Rgb([r, 0, b]);
-    }
+    // println!("{:}, {:}\n{:}, {:}", min_x, max_x, min_y, max_y);
 
-    // A redundant loop to demonstrate reading image data
-    for x in 0..imgx {
-        for y in 0..imgy {
+    for x in min_x..max_x {
+        for y in min_y..max_y {
             let v_a = Point::new(x as f32 - a.x, y as f32 - a.y);
             let v_b = Point::new(x as f32 - b.x, y as f32 - b.y);
             let v_c = Point::new(x as f32 - c.x, y as f32 - c.y);
@@ -50,13 +59,54 @@ fn main() {
             let test_ca = Point::cross_mag(&v_c, &vec_ca) >= 0.0;
 
             if test_ab && test_bc && test_ca {
-                let pixel = imgbuf.get_pixel_mut(x, y);
-                let image::Rgb(data) = *pixel;
-                *pixel = image::Rgb([data[0], 255 as u8, data[2]]);
+                let pixel = buf.get_pixel_mut(x, y);
+                *pixel = color;
             }
+        }
+    }
+}
+
+fn main() {
+    let imgx = 800 as usize;
+    let imgy = 800 as usize;
+
+    // Create a new ImgBuf with width: imgx and height: imgy
+    let mut imgbuf = image::ImageBuffer::new(imgx as u32, imgy as u32);
+
+    // Iterate over the coordinates and pixels of the image
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let r = (0.3 * x as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
+        *pixel = image::Rgb([r, 0, b]);
+    }
+
+    let colors = vec![
+        image::Rgb([148, 0, 211]),
+        image::Rgb([75, 0, 130]),
+        image::Rgb([0, 0, 255]),
+        image::Rgb([0, 255, 0]),
+        image::Rgb([255, 255, 0]),
+        image::Rgb([255, 127, 0]),
+        image::Rgb([255, 0, 0]),
+    ];
+    let mut i = 0;
+    let height = 50 as usize;
+
+    // A redundant loop to demonstrate reading image data
+    for x in (height..imgx).step_by(height) {
+        for y in (height..imgy).step_by(height) {
+            draw_triangle_equilateral(
+                &mut imgbuf,
+                height as f32,
+                Point::new(x as f32, y as f32),
+                i as f32 * std::f32::consts::FRAC_PI_8,
+                colors[i % colors.len()],
+            );
+
+            i += 1;
         }
     }
 
     // Save the image as “fractal.png”, the format is deduced from the path
-    imgbuf.save("fractal.png").unwrap();
+    imgbuf.save("triangles.png").unwrap();
 }
