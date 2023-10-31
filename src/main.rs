59e317862fb96b3a9ff7fd992ccdef0989c3d729
Jacek Grzybowski <totospl99@gmail.com>
2023-11-01 00:43:03 +0100

    generate hexagon function

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,94 @@ pub struct AppState<'a> {
     pub rounder: &'a GridRounder,
 }
 
+fn generate_hexagon(state: &mut AppState, side_len: usize, triangle_height: f32) {
+    state
+        .hexagon_map
+        .insert(state.rounder.get_coord(0.0, 0.0), HexagonKind::Inside123);
+
+    let kinds = [
+        HexagonKind::Vertical12,
+        HexagonKind::Diagonal23,
+        HexagonKind::Diagonal31,
+    ];
+    let mut points = [Point::new(0.0, 0.0); 3];
+
+    let mut angle = -std::f32::consts::FRAC_PI_2;
+    for point in &mut points {
+        point.x = 2.0 * triangle_height / f32::sqrt(3.0) * f32::cos(angle);
+        point.y = 2.0 * triangle_height / f32::sqrt(3.0) * f32::sin(angle);
+        angle += 2.0 * std::f32::consts::FRAC_PI_3;
+    }
+
+    for i in 1..side_len {
+        for (p, k) in std::iter::zip(points, kinds) {
+            state
+                .hexagon_map
+                .insert(state.rounder.get_coord(i as f32 * p.x, i as f32 * p.y), k);
+        }
+    }
+
+    let mut dir_vecs = [Point::new(0.0, 0.0); 6];
+
+    let mut angle = -std::f32::consts::FRAC_PI_2;
+    for vec in &mut dir_vecs {
+        vec.x = (side_len as f32 - 0.1) * 2.0 * triangle_height / f32::sqrt(3.0) * f32::cos(angle);
+        vec.y = (side_len as f32 - 0.1) * 2.0 * triangle_height / f32::sqrt(3.0) * f32::sin(angle);
+        angle += std::f32::consts::FRAC_PI_3;
+    }
+
+    let mut boundary_vecs = [Point::new(0.0, 0.0); 6];
+
+    for i in 0..dir_vecs.len() - 1 {
+        boundary_vecs[i].x = dir_vecs[i + 1].x - dir_vecs[i].x;
+        boundary_vecs[i].y = dir_vecs[i + 1].y - dir_vecs[i].y;
+    }
+    boundary_vecs[dir_vecs.len() - 1].x = dir_vecs[0].x - dir_vecs[dir_vecs.len() - 1].x;
+    boundary_vecs[dir_vecs.len() - 1].y = dir_vecs[0].y - dir_vecs[dir_vecs.len() - 1].y;
+
+    let mut stack = Vec::new();
+    let mut angle = -std::f32::consts::FRAC_PI_2 + std::f32::consts::FRAC_PI_3;
+    for _ in 0..3 {
+        let x = 2.0 * triangle_height / f32::sqrt(3.0) * f32::cos(angle);
+        let y = 2.0 * triangle_height / f32::sqrt(3.0) * f32::sin(angle);
+        stack.push(state.rounder.get_coord(x, y));
+        angle += 2.0 * std::f32::consts::FRAC_PI_3;
+    }
+
+    'outer: while let Some(curr) = stack.pop() {
+        if state.hexagon_map.contains_key(&curr) {
+            continue;
+        }
+        for (vec, start) in std::iter::zip(boundary_vecs, dir_vecs) {
+            let curr_vec = Point::new(curr.0 as f32 - start.x, curr.1 as f32 - start.y);
+            if Point::cross_mag(&curr_vec, &vec) > 0.0 {
+                continue 'outer;
+            }
+        }
+
+        let mut angle = -std::f32::consts::FRAC_PI_2;
+        for _ in 0..3 {
+            let x = curr.0 as f32 + 2.0 * triangle_height / f32::sqrt(3.0) * f32::cos(angle);
+            let y = curr.1 as f32 + 2.0 * triangle_height / f32::sqrt(3.0) * f32::sin(angle);
+            stack.push(state.rounder.get_coord(x, y));
+            angle += 2.0 * std::f32::consts::FRAC_PI_3;
+        }
+
+        let mut checks = [false; 3];
+        for i in 0..3 {
+            let curr_vec = Point::new(curr.0 as f32, curr.1 as f32);
+            checks[i] = Point::cross_mag(&curr_vec, &dir_vecs[2 * i + 1]) > 0.0;
+        }
+        if checks[0] && !checks[1] {
+            state.hexagon_map.insert(curr, HexagonKind::Fill1);
+        } else if checks[1] && !checks[2] {
+            state.hexagon_map.insert(curr, HexagonKind::Fill2);
+        } else if checks[2] && !checks[0] {
+            state.hexagon_map.insert(curr, HexagonKind::Fill3);
+        }
+    }
+}
+
 fn main() {
     let imgx = 851_usize;
     let imgy = 851_usize;
@@ -43,32 +131,7 @@ fn main() {
         rounder: &rounder,
     };
 
-    state
-        .hexagon_map
-        .insert(rounder.get_coord(0.0, 0.0), HexagonKind::Inside123);
-    {
-        let kinds = [
-            HexagonKind::Vertical12,
-            HexagonKind::Diagonal23,
-            HexagonKind::Diagonal31,
-        ];
-        let mut points = [Point::new(0.0, 0.0); 3];
-
-        let mut angle = -std::f32::consts::FRAC_PI_2;
-        for i in 0..points.len() {
-            points[i].x = 2.0 * triangle_height / f32::sqrt(3.0) * f32::cos(angle);
-            points[i].y = 2.0 * triangle_height / f32::sqrt(3.0) * f32::sin(angle);
-            angle += 2.0 * std::f32::consts::FRAC_PI_3;
-        }
-
-        for i in 1..6 {
-            for (j, p) in points.iter().enumerate() {
-                state
-                    .hexagon_map
-                    .insert(rounder.get_coord(i as f32 * p.x, i as f32 * p.y), kinds[j]);
-            }
-        }
-    }
+    generate_hexagon(&mut state, 6, triangle_height);
 
     for (coord, kind) in state.hexagon_map.clone() {
         draw_hexagon(
